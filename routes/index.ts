9a73a1f45d6781108c38159768969849468baf80
mubasir umbi <mubasirumbi@gmail.com>
2025-01-09 19:50:29 +0530

    tailwind setup completed

--- routes/index.ts
+++ routes/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'welcome to Auxwall' });
+  res.render('index', { title: 'Auxwall' });
 });
 
 export default router;
