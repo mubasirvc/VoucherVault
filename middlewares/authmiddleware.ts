1565ba8beaa82b912914672d048d4122d9dac37a
mubasir umbi <mubasirumbi@gmail.com>
2025-01-12 20:19:29 +0530

    middlewares added

--- middlewares/authmiddleware.ts
+++ middlewares/authmiddleware.ts
@@ -7,6 +7,14 @@ export const requireAuth = (req: Request, res: Response, next: NextFunction): vo
   next();
 };
 
+export const loginCheck = (req: Request, res: Response, next: NextFunction): void => {
+  if ((req.session as any).user) {
+    res.redirect('/home')
+    return
+  }
+  next();
+};
+
 export const noCache = (req: Request, res: Response, next: NextFunction): void => {
   res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
   res.set("Pragma", "no-cache");
